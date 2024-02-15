9aa20b578132ff6814b936f668e2ab5a036ac0b7
gabsfrmarqs <gabrielfrmarques@outlook.com>
2024-02-15 11:57:37 -0300

    challenge finished

---
+++ chapter3/challenge.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+#$HOME = user home directory
+#$PATH = directories where commands are searched
+#$HOSTNAME = hostname of the macine
+#$SHELL = shell being used
+#$USER = user of this session
+#$TERM = type of terminal used
+
+echo "Ayo have some envs"
+echo "$HOSTNAME"
+echo "$USER"
+echo "$HOME"
+exit 0
