caab76f22c3ccb88ecc34b0567170160f6408b5c
gabsfrmarqs <gabrielfrmarques@outlook.com>
2024-02-15 11:56:21 -0300

    added env examples

--- chapter3/readingenv.sh
+++ chapter3/readingenv.sh
@@ -1,5 +1,12 @@
 #/usr/bin/env bash
 
+#$HOME = user home directory
+#$PATH = directories where commands are searched
+#$HOSTNAME = hostname of the macine
+#$SHELL = shell being used
+#$USER = user of this session
+#$TERM = type of terminal used
+
 echo "PATH is: $PATH"
 echo "Terminal is $TERM"
 echo "Editor is: $EDITOR"
