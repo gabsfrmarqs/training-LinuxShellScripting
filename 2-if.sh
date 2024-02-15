6ca86b876d539cd7295a5ae06bd91331b3f6300f
gabsfrmarqs <gabrielfrmarques@outlook.com>
2024-02-15 10:56:51 -0300

    second exercise

---
+++ 2-if.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+COLOR=$1
+
+if [$COLOR = "blue" ]
+then
+	echo "bluuuuuuue"
+else
+	echo "WRONG HAHAHAAHAHAHA"
+fi
+
+#it doesn't make sense, i know, BUT I'M LEARNING AND IT WORKS SO THAT'S WHAT MATTERS AND
